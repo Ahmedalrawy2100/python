d4a3e2e57670b6457b18d2d5b40108aba5f2ebf3
Ahmed Alrawy <ahmedalrawy82@gmail.com>
2026-07-17 01:08:59 +0300

    Add Day 3

---
+++ task-while.py
@@ -0,0 +1,18 @@
+# while loop
+# i = 0
+# while i < 10:
+#     print(i)
+#     i+=1
+# print("Done with the loop")
+
+
+#while list
+names = ["ahmed", "ali", "omar", "sara", "mona", "laila", "nour", "youssef", "hassan", "alrawy"]
+
+i = 0
+while i < len(names):
+
+    print(f"#{str(1+i).zfill(2)}: {names[i]}")
+    i+=1
+
+print("Done with the loop")
