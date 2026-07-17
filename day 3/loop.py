1a298f73d78a9dd15e8bc5877d2c42f5b868a565
ahmed alrawy <Ahmedalrawy82@gmail.com>
2026-07-17 10:55:05 +0300

    Uncomment loop to print numbers excluding 10
    Uncommented the loop to print numbers except 10.

--- day 3/loop.py
+++ day 3/loop.py
@@ -2,10 +2,10 @@
 
 Numbers=[1,2,3,4,5,6,7,8,9,10,11,12]
 
-# for num in Numbers:
-#     if num == 10 :
-#         continue
-#     print(num)
+for num in Numbers:
+    if num == 10 :
+        continue
+    print(num)
 
 # #------------
 
